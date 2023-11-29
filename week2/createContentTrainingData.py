034fe48fc062520774b3620235e6750ff01803bb
GuilhermGuitte <guilherme.guitte@gmail.com>
2023-11-29 17:07:58 +0100

    feat(lvl-1): filter labels with less than 500 product associated

--- week2/createContentTrainingData.py
+++ week2/createContentTrainingData.py
@@ -1,6 +1,8 @@
 import argparse
 import multiprocessing
 import glob
+from collections import Counter
+
 from tqdm import tqdm
 import os
 import xml.etree.ElementTree as ET
@@ -20,14 +22,14 @@ general.add_argument("--output", default="/workspace/datasets/fasttext/output.fa
 general.add_argument("--label", default="id", help="id is default and needed for downsteam use, but name is helpful for debugging")
 
 # IMPLEMENT: Setting min_products removes infrequent categories and makes the classifier's task easier.
-general.add_argument("--min_products", default=0, type=int, help="The minimum number of products per category (default is 0).")
+general.add_argument("--min_products", default=500, type=int, help="The minimum number of products per category (default is 0).")
 
 args = parser.parse_args()
 output_file = args.output
 path = Path(output_file)
 output_dir = path.parent
 if os.path.isdir(output_dir) == False:
-        os.mkdir(output_dir)
+    os.mkdir(output_dir)
 
 if args.input:
     directory = args.input
@@ -44,26 +46,40 @@ def _label_filename(filename):
     for child in root:
         # Check to make sure category name is valid and not in music or movies
         if (child.find('name') is not None and child.find('name').text is not None and
-            child.find('categoryPath') is not None and len(child.find('categoryPath')) > 0 and
-            child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text is not None and
-            child.find('categoryPath')[0][0].text == 'cat00000' and
-            child.find('categoryPath')[1][0].text != 'abcat0600000'):
-              # Choose last element in categoryPath as the leaf categoryId or name
-              if names_as_labels:
-                  cat = child.find('categoryPath')[len(child.find('categoryPath')) - 1][1].text.replace(' ', '_')
-              else:
-                  cat = child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text
-              # Replace newline chars with spaces so fastText doesn't complain
-              name = child.find('name').text.replace('\n', ' ')
-              labels.append((cat, transform_name(name)))
+                child.find('categoryPath') is not None and len(child.find('categoryPath')) > 0 and
+                child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text is not None and
+                child.find('categoryPath')[0][0].text == 'cat00000' and
+                child.find('categoryPath')[1][0].text != 'abcat0600000'):
+            # Choose last element in categoryPath as the leaf categoryId or name
+            if names_as_labels:
+                cat = child.find('categoryPath')[len(child.find('categoryPath')) - 1][1].text.replace(' ', '_')
+            else:
+                cat = child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text
+            # Replace newline chars with spaces so fastText doesn't complain
+            name = child.find('name').text.replace('\n', ' ')
+            labels.append((cat, transform_name(name)))
     return labels
 
 if __name__ == '__main__':
     files = glob.glob(f'{directory}/*.xml')
-    print("Writing results to %s" % output_file)
     with multiprocessing.Pool() as p:
         all_labels = tqdm(p.imap(_label_filename, files), total=len(files))
+        all_labels_count = tqdm(p.imap(_label_filename, files), total=len(files))
         with open(output_file, 'w') as output:
+            labels_count = []
+
+            print("Counting categories...")
+            for label_list in all_labels_count:
+                for (cat, name) in label_list:
+                    labels_count.append(cat)
+
+            labels_count = Counter(labels_count)
+            labels_count = [filtered_label for filtered_label, count in labels_count.items() if int(count) >= int(min_products)]
+
+            print(f"Minimum products associated: %i - Total number of labels found: %i" % (min_products, len(labels_count)))
+            print(labels_count)
+
+            print("Writing results to %s" % output_file)
             for label_list in all_labels:
                 for (cat, name) in label_list:
-                    output.write(f'__label__{cat} {name}\n')
+                    output.write(f'__label__{cat} {name}\n') if cat in labels_count else None
